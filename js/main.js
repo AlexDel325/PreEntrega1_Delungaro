060b6e0f7d06b461da50b7348e35bc54fe9bdd8d
AlexDel <alexis.delungaro325@gmail.com>
2023-01-18 13:27:03 -0300

    Agrego pokemones a la lista con una api

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 // class con constructor (permite crear un objeto "Usuario")
 class Usuario {
-    constructor(nombre, edad, profecion) {
+    constructor(nombre, apellido, edad, profecion, correo, mascota) {
         this.nombre = nombre;
+        this.apellido = apellido;
         this.edad = edad;
         this.profecion = profecion;
+        this.correo = correo;
+        this.mascota = mascota
     }
 };
 
@@ -11,6 +14,7 @@ class Usuario {
 let displayMenu = document.getElementById("display-menu"),
     closeMenu = document.getElementById("close-menu"),
     confirmar = document.getElementById("confirmar"),
+    borrarTodo = document.getElementById("delete-all"),
     form = document.getElementById("formulario"),
     tabla = document.getElementById("componentes"),
     list = document.getElementById("lista"),
@@ -24,15 +28,20 @@ let listaUsuarios = [],
 //funciones
 const crearUsuario = () => {
     let nombre = document.getElementById("nombre").value,
+        apellido = document.getElementById("apellido").value,
         edad = parseInt(document.getElementById("edad").value),
-        profecion = document.getElementById("equipo").value;
+        profecion = document.getElementById("equipo").value,
+        correo = document.getElementById("correo").value,
+        mascota = (Math.floor(Math.random() * (1008 - 1) + 1));
 
     if (edad < 18) {
-        console.log('el usuario debe ser mayor de edad');
-
+        swal({
+            title: "Oh no!",
+            text: "El usuario debe ser mayor de edad!",
+            icon: "error",
+        });
     } else if (edad >= 18) {
-
-        let usuarioNuevo = new Usuario(nombre, edad, profecion);
+        let usuarioNuevo = new Usuario(nombre, apellido, edad, profecion, correo, mascota);
         listaUsuarios.push(usuarioNuevo);
     }
 
@@ -56,39 +65,52 @@ const crearlista = () => {
 const crearTabla = () => {
     listaUsuarios.forEach((Usuario) => {
         todosLosUsuarios.push(Usuario);
-
-        let row = document.createElement('tr'),
-            nomreCell = document.createElement('td'),
-            edadCell = document.createElement('td'),
-            profecionCell = document.createElement('td');
-
-        nomreCell.textContent = Usuario.nombre;
-        edadCell.textContent = Usuario.edad;
-        profecionCell.textContent = Usuario.profecion;
-
-        tabla.appendChild(row);
-        row.appendChild(nomreCell);
-        row.appendChild(edadCell);
-        row.appendChild(profecionCell);
     });
+
 }
 
 const imprimirStorage = () => {
     todosLosUsuarios = JSON.parse(localStorage.getItem('todosLosUsuarios'));
+
     todosLosUsuarios.forEach((Usuario) => {
         let row = document.createElement('tr'),
             nomreCell = document.createElement('td'),
+            apellidoCell = document.createElement('td'),
             edadCell = document.createElement('td'),
-            profecionCell = document.createElement('td');
+            profecionCell = document.createElement('td'),
+            correoCell = document.createElement('td'),
+            pokeCell = document.createElement('td');
 
         nomreCell.textContent = Usuario.nombre;
+        apellidoCell.textContent = Usuario.apellido;
         edadCell.textContent = Usuario.edad;
         profecionCell.textContent = Usuario.profecion;
+        correoCell.textContent = Usuario.correo;
 
         tabla.appendChild(row);
         row.appendChild(nomreCell);
+        row.appendChild(apellidoCell);
         row.appendChild(edadCell);
         row.appendChild(profecionCell);
+        row.appendChild(correoCell);
+
+        let url = `https://pokeapi.co/api/v2/pokemon/${Usuario.mascota}/`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                pokeCell.textContent = data.name;
+                row.appendChild(pokeCell);
+                pokeCell.className = "pokemon";
+
+                pokeCell.onclick = () => {
+                    swal({
+                        title: data.name,
+                        icon: data.sprites.front_default,
+                    });
+                }
+            })
+            .catch(err => console.log * (err))
     });
 }
 
@@ -117,9 +139,11 @@ todosLosUsuarios.sort((a, b) => {
         return -1;
     }
     return 0;
-})
+});
 
 //manejo de DOM
+
+
 if (JSON.parse(localStorage.getItem('todosLosUsuarios') != null)) {
     imprimirStorage();
 }
@@ -136,4 +160,30 @@ confirmar.onclick = () => {
     form.style.display = "none";
     crearTabla();
     guardarStorage();
+    window.location.reload();
+}
+
+borrarTodo.onclick = () => {
+    swal({
+            title: "Estas Seguro?",
+            text: "No podras recuperar tus datos una vez sean borrardos",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Poof! ahora tu tabla esta vacia!", {
+                    icon: "success",
+                    buttons: false,
+                });
+                localStorage.clear();
+                setTimeout((function () {
+                    window.location.reload();
+                }), 3000);
+            } else {
+                swal("La tabla permanecera completa hasta que decidas borrarla");
+            }
+        });
+
 }
